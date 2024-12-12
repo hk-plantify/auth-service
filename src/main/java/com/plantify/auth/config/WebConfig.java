5affa8600f7e882d784d755e9b935b95c7481baf
seunzu <0123suh@gmail.com>
2024-12-12 18:47:06 +0900

    feat: webConfig 수정

--- src/main/java/com/plantify/auth/config/WebConfig.java
+++ src/main/java/com/plantify/auth/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.plantify.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${client.base.url}")
+    private String clientBaseUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", clientBaseUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

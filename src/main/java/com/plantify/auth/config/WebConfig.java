0dc9ffaaa1edc64b7407b4ba43e70907dbabe839
seunzu <0123suh@gmail.com>
2024-12-15 22:19:13 +0900

    feat: clientUrl 수정

--- src/main/java/com/plantify/auth/config/WebConfig.java
+++ src/main/java/com/plantify/auth/config/WebConfig.java
@@ -10,10 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${client.base.url}")
     private String clientBaseUrl;
 
+    @Value("${client.pay.url}")
+    private String clientPayUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", clientBaseUrl)
+                .allowedOrigins("http://localhost:3000", clientBaseUrl, clientPayUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
